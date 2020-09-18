a245aa6e5d0e6faa4aba9d6a2679b9dad2224c63
RomilaDeck <54414765+RomilaDeck@users.noreply.github.com>
2020-09-18 12:43:20 +0530

    Integrated API

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from flask import Flask, render_template, request
+from flask import Flask,render_template, request
 
 
 app=Flask(__name__)
@@ -10,20 +10,16 @@ app=Flask(__name__)
 def index():
 
     if request.method == "POST":
-        city = request.form['city']
-        country = request.form['country']
-        api_key = "870095279633b0570e19250d31ee5e74"
+    	city = request.form['city']
+    	api_key = "870095279633b0570e19250d31ee5e74"
 
-        weather_url = requests.get(
-            f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city},{country}&units=imperial')
-
-        weather_data = weather_url.json()
-
-        temp = round(weather_data['main']['temp'])
-        humidity = weather_data['main']['humidity']
-        wind_speed = weather_data['wind']['speed']
-
-        return render_template("result.html", temp=temp, humidity=humidity, wind_speed=wind_speed, city=city)
-
+    	if city:
+    		weather_url = requests.get(
+	            f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city}&units=imperial')
+    		weather_data = weather_url.json()
+    		temp = round(weather_data['main']['temp'])
+    		humidity = weather_data['main']['humidity']
+    		wind_speed = weather_data['wind']['speed']
+    		return render_template("result.html", temp=temp, humidity=humidity, wind_speed=wind_speed, city=city)
     return render_template("index.html")
 
